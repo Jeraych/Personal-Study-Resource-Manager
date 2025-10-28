dae6f4a594d41fd0705b102e7907a705b251d5f8
Jeraych <Jacky2022332467@gmail.com>
2025-10-29 01:20:39 +1300

    Added Mavenwrapper for parent folder to open project easily

--- backend/src/main/java/com/jeraych/psrm/backend/PsrmApplication.java
+++ backend/src/main/java/com/jeraych/psrm/backend/PsrmApplication.java
@@ -1,13 +1,19 @@
 package com.jeraych.psrm.backend;
 
+import com.jeraych.psrm.client.App;
+import javafx.application.Platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
 
 @SpringBootApplication
 public class PsrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsrmApplication.class, args);
+		Application.launch(App.class);
+		Platform.exit();
+		System.exit(0);
 	}
 
 }
